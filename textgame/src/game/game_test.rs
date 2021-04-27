dcc4fb28a86c8ca2ab06920d4f8a0e4866231c07
mijies <seiji3030@protonmail.com>
2021-04-27 20:35:35 +0900

    add check_item_test()

--- textgame/src/game/game_test.rs
+++ textgame/src/game/game_test.rs
@@ -1,9 +1,35 @@
 use crate::error::GameError;
+use crate::item::{ItemList, Item};
+use crate::room::Room;
 use super::*;
 
 #[test]
 fn check_item_test() -> Result<(), GameError>{
-    let mut game = Game::new();
-    assert_eq!(check_item(&mut game)?, ());
+    assert_eq!(
+        check_item(&mut Game::new())?, ()
+    );
+
+    let mut items = ItemList::new(Vec::new());
+    for _ in 1..Room::item_count() {
+        items.push((Room::Euler, "item" as Item));
+    }
+    assert_eq!(
+        check_item(&mut Game {
+            room: Room::PointAtInfinity,
+            items: items,
+        })?, ()
+    );
+
+    let mut items = ItemList::new(Vec::new());
+    for _ in 0..Room::item_count() {
+        items.push((Room::Euler, "item" as Item));
+    }
+    assert_eq!(
+        check_item(&mut Game {
+            room: Room::PointAtInfinity,
+            items: items,
+        }), Err(GameError::Over)
+    );
+
     Ok(())
 }
